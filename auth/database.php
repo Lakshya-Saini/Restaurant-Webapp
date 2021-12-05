73696dcf8dbe8041e5022dcdadb69fd433598bd1
Lakshya Saini <46291816+Lakshya-Saini@users.noreply.github.com>
2021-12-05 18:33:08 +0530

    db details updated

--- auth/database.php
+++ auth/database.php
@@ -1,5 +1,5 @@
 <?php
 	
-	$connect = mysqli_connect('localhost', 'root', '', 'restaurant');
+	$connect = mysqli_connect($_ENV['SERVER'], $_ENV['USERNAME'], $_ENV['PASSWORD'], $_ENV['DB_NAME']);
 
 ?>
